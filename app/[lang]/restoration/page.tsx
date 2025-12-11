0c15c9a554fb59f3325b73e91ecb4f4068753e22
Xavier <joubertxavier@gmail.com>
2025-12-11 18:27:48 +0100

    add new page

---
+++ app/[lang]/restoration/page.tsx
@@ -0,0 +1,7 @@
+export default function RestorationPage() {
+  return (
+    <div className="flex flex-col w-full h-auto min-h-[100dvh] justify-center items-center overflow-scroll">
+      <h1>Restoration</h1>
+    </div>
+  );
+}
