aefdc4d56522f82371f42423020878efbe51009e
Xavier <joubertxavier@gmail.com>
2025-12-12 17:05:56 +0100

    add for sale section

---
+++ components/molecules/homepageSections/forSaleSection.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const ForSaleSection = ({
+  dict,
+  usedLanguage,
+}: {
+  dict: any;
+  usedLanguage: string;
+}) => {
+  return (
+    <section className="flex flex-col w-full h-auto justify-center items-center overflow-scroll"></section>
+  );
+};
+
+export default ForSaleSection;
