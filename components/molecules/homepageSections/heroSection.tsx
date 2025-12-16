f70762235c72e655308711be4333c65a476ccbe1
Xavier <joubertxavier@gmail.com>
2025-12-16 17:55:13 +0100

    update UI Hero

--- components/molecules/homepageSections/heroSection.tsx
+++ components/molecules/homepageSections/heroSection.tsx
@@ -10,22 +10,15 @@ const HeroSection = ({ dict }: { dict: any }) => {
         className="w-full h-[100dvh] object-cover object-center"
       />
       <div className="absolute bottom-24 md:bottom-48 flex w-full h-full px-[2%] justify-center items-end z-30">
-        <div className="flex flex-col w-full max-w-[1000px] h-auto gap-x-4 ">
-          {dict?.landing?.hero.title.map((title: string, index: number) => (
-            <div
-              className="flex flex-row flex-wrap w-auto h-auto justify-center items-center text-center"
-              key={index}
-            >
-              <h1
-                className={`text-shadow-black text-shadow-sm font-semibold  ${
-                  index > 0 ? "text-red-500" : "text-white"
-                } text-[20px] sm:text-[32px] md:text-[48px] text-center tracking-widest
+        <div className="flex flex-col w-full max-w-[800px] h-auto gap-x-4 ">
+          <h1
+            className={`text-shadow-black text-shadow-sm font-semibold text-white
+              text-[20px] sm:text-[32px] md:text-[48px] text-center tracking-widest text-pretty
                 `}
-              >
-                {title}
-              </h1>
-            </div>
-          ))}
+          >
+            {dict?.landing?.hero.title[0]}{" "}
+            <span className="text-red-500">{dict?.landing?.hero.title[1]}</span>
+          </h1>
         </div>
       </div>
     </section>
