b28bf1ac900bb74f0761bbbc5bfd8cac48fa7885
Xavier <joubertxavier@gmail.com>
2025-12-17 08:19:15 +0100

    add pages

---
+++ app/[lang]/about-us/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutUsPage() {
+  return (
+    <div className="flex flex-col w-full h-auto min-h-[100dvh] justify-center items-center overflow-scroll">
+      <h1>About Us</h1>
+    </div>
+  );
+}
