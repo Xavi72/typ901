07fc706b9c7ea53f8490ef6f92eddff02d7c2dff
Xavier <joubertxavier@gmail.com>
2025-12-22 16:28:30 +0100

    update UI

--- components/views/homePage.tsx
+++ components/views/homePage.tsx
@@ -19,7 +19,8 @@ export default async function HomePage({
   const dict: any = await getDictionary(lang);
 
   return (
-    <div className="snap-y snap-mandatory overflow-y-scroll h-screen bg-gray-800">
+    <div className="snap-y snap-mandatory overflow-y-scroll h-screen bg-gray-800 relative">
+      <div className="fixed top-0 left-0 w-full h-20 bg-gradient-to-b from-gray-800 to-transparent z-30"></div>
       <div className="snap-always snap-start h-[100dvh]">
         <HeroSection dict={dict} />
       </div>
