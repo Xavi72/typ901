9aaeec23bfe5cb041b5e7e471e3141cb101783c2
Xavier <joubertxavier@gmail.com>
2026-01-30 18:08:16 +0100

    update UI

--- components/molecules/homepageSections/heroSection.tsx
+++ components/molecules/homepageSections/heroSection.tsx
@@ -10,7 +10,7 @@ const HeroSection = ({ dict }: { dict: any }) => {
         className="w-full h-[100dvh] object-cover object-center"
       />
       <div className="absolute bottom-24 md:bottom-48 flex w-full h-full px-[2%] justify-center items-end z-30">
-        <div className="flex flex-col w-full max-w-[800px] h-auto gap-x-4 ">
+        <div className="flex flex-col w-full max-w-[340px] sm:max-w-[520px] md:max-w-[800px] h-auto gap-x-4 ">
           <h1
             className={`text-shadow-black text-shadow-sm font-semibold text-white
               text-[20px] sm:text-[32px] md:text-[48px] text-center tracking-widest text-pretty
